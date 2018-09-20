2d854893d8f85414aa37edb5d9c0ce94bdb62d56
Rasmus Lelumees <rasmus.lelumees@gmail.com>
2018-09-20 23:39:27 +0300

    Clean up dependencies, add primitive tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import sinon from 'sinon';
 import App from './index.js';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('App Snapshot', () => {
+    it('renders', () => {
+        const component = renderer.create(<App />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe('App Component', () => {
+    it('renders without crashing', () => {
+        shallow(<App />);
+    });
+
+    it('renders welcome message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2 className='App-title'>Welcome to ContactBook!</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('simulates search event', () => {
+        const handleChangeSpy = sinon.spy(App.prototype, 'handleSearchInputChange');
+        const wrapper = mount(<App />);
+        const event = {target: {name: 'App-searchbar-input', value: 'somequery'}};
+
+        wrapper.find('.App-searchbar-input').simulate('change', event);
+        expect(handleChangeSpy.calledOnce).toEqual(true);
+    });
 });
